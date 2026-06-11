fc237098dd702e038650e43e3fe742ad1c41308f
Sanjay <sanjutech9959@gmail.com>
2026-06-11 13:02:53 +0530

    fix: suppress React Router v6 future flag warnings

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
   return (
     <ThemeProvider>
       <SavedProvider>
-        <BrowserRouter>
+        <BrowserRouter  future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <div className="min-h-screen flex flex-col">
             <Navbar />
             <main className="flex-1">
